43466c9fce94eaeb48c1cf3252054d605dbcc69a
Mohan-Balaji <bmohanbalaji1976@gmail.com>
2025-02-23 23:38:38 +0530

    Double Linked List

---
+++ DoubleLinkedList.java
@@ -0,0 +1,14 @@
+public class DoubleLinkedList{
+
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=next;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
