d91c8bb60ca250082f937557c9cc2ca9a49f6d2d
Mohan-Balaji <bmohanbalaji1976@gmail.com>
2025-02-24 10:37:19 +0530

    Adding Element at End of the Linked List

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -11,4 +11,4 @@ public class DoubleLinkedList{
     public static void main(String[] args) {
         
     }
-}
+}   
---
+++ LinkedListInsertionAtEnd.java
@@ -0,0 +1,32 @@
+public class LinkedListInsertionAtEnd {
+     public static class Node{ 
+            int data;
+            Node next ; 
+            Node(int data){
+                this.data=data;
+                this.next=next;
+            }
+        }
+    
+    class Solution {
+
+       
+        // Function to insert a node at the end of the linked list.
+        Node insertAtEnd(Node head, int x) {
+            Node newNode = new Node(x);
+        if(head==null){
+           return newNode;
+        } 
+        Node temp = head;
+        while(temp.next != null){
+            temp=temp.next;
+        }    
+        temp.next=newNode;
+      return head;  
+    } 
+    }
+    public static void main(String[] args) {
+        LinkedListInsertionAtEnd ll = new LinkedListInsertionAtEnd();
+        ll.insertAtEnd(5);
+    }
+}
